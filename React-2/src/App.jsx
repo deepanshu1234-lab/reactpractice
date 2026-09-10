3033c8e52650fbe2d4e3dee096e8039f5ef4a6ac
deepanshu <deepanshugarg2507@gmail.com>
2026-09-10 10:56:39 +0530

    Initial commit

---
+++ React-2/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Contact from './Contact.jsx'
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>Welcome to the App</h1>
+      <Contact />
+    </div>
+  )
+}
+
+export default App
